637757e21dd36ed4a3b61856e5b77f33870e486a
Robby Singh <kingraj2323@gmail.com>
2020-12-10 19:06:05 -0600

    Made changes

--- celsius_conversion.py
+++ celsius_conversion.py
@@ -1,1 +1,4 @@
-celsius = int(input(Please enter a Celsius temperature: ))
+celsius = int(input('Please enter a Celsius temperature: '))
+fahrenheit = (celsius * 9/5) + 32 
+print('The temperature in C is %s' % (celsius))
+print(str(fahrenheit) + ' F')
